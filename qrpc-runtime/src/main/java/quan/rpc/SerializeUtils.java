ae71843c045a98c6e4071842d43a8549a52a9729
quanchangnai <quanchangnai@126.com>
2023-12-12 15:44:13 +0800

    对象序列化工具优化

--- qrpc-runtime/src/main/java/quan/rpc/SerializeUtils.java
+++ qrpc-runtime/src/main/java/quan/rpc/SerializeUtils.java
@@ -5,17 +5,10 @@ import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 import org.apache.commons.lang3.mutable.MutableObject;
-import org.apache.commons.pool2.BasePooledObjectFactory;
-import org.apache.commons.pool2.PooledObject;
-import org.apache.commons.pool2.PooledObjectFactory;
-import org.apache.commons.pool2.impl.DefaultPooledObject;
-import org.apache.commons.pool2.impl.GenericObjectPool;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.time.Duration;
 
 /**
  * 序列化工具
@@ -25,50 +18,14 @@ public class SerializeUtils {
 
     private static final Schema<MutableObject> rootSchema = RuntimeSchema.getSchema(MutableObject.class);
 
-    private static final ThreadLocal<LinkedBuffer> localBuffer = ThreadLocal.withInitial(LinkedBuffer::allocate);
-
-    private static final GenericObjectPool<LinkedBuffer> bufferPool;
-
-    static {
-        int maxTotal = Integer.parseInt(System.getProperty("qrpc.serialize.pooled_buffer_max_total", "32"));
-
-        GenericObjectPoolConfig<LinkedBuffer> poolConfig = new GenericObjectPoolConfig<>();
-        poolConfig.setMaxTotal(Math.max(maxTotal, 8));
-        poolConfig.setMinIdle(4);
-        poolConfig.setSoftMinEvictableIdleDuration(Duration.ofMinutes(5));
-
-        PooledObjectFactory<LinkedBuffer> factory = new BasePooledObjectFactory<LinkedBuffer>() {
-            @Override
-            public LinkedBuffer create() {
-                return LinkedBuffer.allocate();
-            }
-
-            @Override
-            public PooledObject<LinkedBuffer> wrap(LinkedBuffer obj) {
-                return new DefaultPooledObject<>(obj);
-            }
-        };
-
-        bufferPool = new GenericObjectPool<>(factory, poolConfig);
-    }
+    private static final ThreadLocal<LinkedBuffer> localBuffer = ThreadLocal.withInitial(() -> LinkedBuffer.allocate(2048));
 
 
     private static LinkedBuffer allocBuffer(boolean useLocalBuffer) {
         if (useLocalBuffer) {
             return localBuffer.get();
         } else {
-            try {
-                return bufferPool.borrowObject();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static void releaseBuffer(LinkedBuffer buffer, boolean useLocalBuffer) {
-        buffer.clear();
-        if (!useLocalBuffer) {
-            bufferPool.returnObject(buffer);
+            return LinkedBuffer.allocate();
         }
     }
 
@@ -84,10 +41,7 @@ public class SerializeUtils {
         return serialize(obj, useLocalBuffer());
     }
 
-    /**
-     * 序列化对象
-     */
-    public static <T> byte[] serialize(T obj, boolean useLocalBuffer) {
+    static <T> byte[] serialize(T obj, boolean useLocalBuffer) {
         LinkedBuffer buffer = allocBuffer(useLocalBuffer);
 
         byte[] bytes;
@@ -95,7 +49,7 @@ public class SerializeUtils {
         try {
             bytes = ProtostuffIOUtil.toByteArray(new MutableObject<>(obj), rootSchema, buffer);
         } finally {
-            releaseBuffer(buffer, useLocalBuffer);
+            buffer.clear();
         }
 
         return bytes;
@@ -108,16 +62,13 @@ public class SerializeUtils {
         serialize(obj, os, useLocalBuffer());
     }
 
-    /**
-     * 序列化对象
-     */
-    public static <T> void serialize(T obj, OutputStream os, boolean useLocalBuffer) throws IOException {
+    static <T> void serialize(T obj, OutputStream os, boolean useLocalBuffer) throws IOException {
         LinkedBuffer buffer = allocBuffer(useLocalBuffer);
 
         try {
             ProtostuffIOUtil.writeTo(os, new MutableObject<>(obj), rootSchema, buffer);
         } finally {
-            releaseBuffer(buffer, useLocalBuffer);
+            buffer.clear();
         }
     }
 
@@ -138,10 +89,7 @@ public class SerializeUtils {
         return deserialize(is, useLocalBuffer());
     }
 
-    /**
-     * 反序列化对象
-     */
-    public static <T> T deserialize(InputStream is, boolean useLocalBuffer) throws IOException {
+    static <T> T deserialize(InputStream is, boolean useLocalBuffer) throws IOException {
         LinkedBuffer buffer = allocBuffer(useLocalBuffer);
 
         try {
@@ -149,7 +97,7 @@ public class SerializeUtils {
             ProtostuffIOUtil.mergeFrom(is, rootObject, rootSchema, buffer);
             return (T) rootObject.getValue();
         } finally {
-            releaseBuffer(buffer, useLocalBuffer);
+            buffer.clear();
         }
     }
 
@@ -161,13 +109,9 @@ public class SerializeUtils {
         return deserialize(bytes);
     }
 
-    /**
-     * 复制对象
-     */
-    public static <T> T clone(T obj, boolean useLocalBuffer) {
+    static <T> T clone(T obj, boolean useLocalBuffer) {
         byte[] bytes = serialize(obj, useLocalBuffer);
         return deserialize(bytes);
     }
-
 
 }
