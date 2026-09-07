584c43fae2d7b6971414a58928cd2e657ff114d9
quanchangnai <changnai.quan@funplus.com>
2026-09-07 17:04:07 +0800

    修复报错，高版本JDK获取元素所在源文件名以及行号

--- qrpc-generator/src/main/java/quan/rpc/JavacSourceLineResolver.java
+++ qrpc-generator/src/main/java/quan/rpc/JavacSourceLineResolver.java
@@ -12,18 +12,31 @@ import javax.lang.model.element.Element;
  */
 public class JavacSourceLineResolver extends SourceLineResolver {
 
+    private boolean resolveFailed;
+
     /**
      * 获取元素所在源文件名以及行号
      * 参考 jdk.javadoc.internal.tool.Messager#getDiagSource(Element)
      */
     @Override
     public String resolveSourceLine(ProcessingEnvironment processingEnv, Element element) {
-        JavacTrees javacTrees = JavacTrees.instance(processingEnv);
-        CompilationUnitTree compilationUnit = javacTrees.getPath(element).getCompilationUnit();
-        long sourcePosition = javacTrees.getSourcePositions().getStartPosition(compilationUnit, javacTrees.getTree(element));
-        long lineNumber = compilationUnit.getLineMap().getLineNumber(sourcePosition);
-        String fileName = PathFileObject.getSimpleName(compilationUnit.getSourceFile());
-        return fileName + ":" + lineNumber;
+        if (resolveFailed) {
+            return null;
+        }
+
+        try {
+            JavacTrees javacTrees = JavacTrees.instance(processingEnv);
+            CompilationUnitTree compilationUnit = javacTrees.getPath(element).getCompilationUnit();
+            long sourcePosition = javacTrees.getSourcePositions().getStartPosition(compilationUnit, javacTrees.getTree(element));
+            long lineNumber = compilationUnit.getLineMap().getLineNumber(sourcePosition);
+            String fileName = PathFileObject.getSimpleName(compilationUnit.getSourceFile());
+            return fileName + ":" + lineNumber;
+        } catch (Throwable e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            resolveFailed = true;
+            return null;
+        }
     }
 
 }
